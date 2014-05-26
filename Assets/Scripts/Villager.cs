0c5c24f96aa4984373756bd2fa1b64d3cecc6d20
flameiguana <flameiguana@gmail.com>
2014-05-25 21:58:07 -0700

    Added house and male peasant with panic animation

---
+++ Assets/Scripts/Villager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Villager : MonoBehaviour {
+
+	private Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		animator.SetBool("Panic", true);
+	}
+
+	void OnTriggerExit2D()
+	{
+
+	}
+}
