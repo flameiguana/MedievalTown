8ce64f4d326fb85b4d33ecc70dc19a31b264f075
Eric Anderson <eanders1@ucsc.edu>
2014-05-25 22:59:41 -0700

    Modified some things.
    rolling around. Peasants are heavy. Added Utility func, added some
    layers, hacky fix to dropping peasants

---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Utility {
+	public delegate void doThis();
+
+	public static IEnumerator wait(float t, doThis d){
+		yield return new WaitForSeconds(t);
+		d ();
+	}
+}
--- Assets/Scripts/Wessbat.cs
+++ Assets/Scripts/Wessbat.cs
@@ -3,25 +3,19 @@ using System.Collections;
 
 public class Wessbat : MonoBehaviour {
 	private GameObject pickedUp = null; //object being held
-	[SerializeField] private Vector3 objectDrift = new Vector3(0f,-0.5f,0f);
 
 	void Start () {
 	
 	}
-	
 
 	void Update () {
-		if (pickedUp) {
-			pickedUp.transform.localPosition = Vector3.zero + objectDrift;
-		}
-
 		if (Input.GetKeyDown (KeyCode.F)) {
 			Drop ();
 		}
 		//control scheme goes here
 	}
 
-	void OnCollisionEnter2D(Collision2D target){
+	void OnCollisionStay2D(Collision2D target){
 		switch (target.gameObject.layer) {
 		case 8:							//Enemy layer
 			if(Input.GetKey (KeyCode.E))
@@ -35,18 +29,30 @@ public class Wessbat : MonoBehaviour {
 	void PickUp(GameObject target){		//Pick up the object
 		if (pickedUp != null)
 			return;
+		SpringJoint2D joint = target.AddComponent<SpringJoint2D> ();
+		joint.connectedBody = rigidbody2D;
+		joint.frequency = 20f;
+		joint.distance = 0.01f;
+		joint.anchor = new Vector2 (0, 1);	//inline anchor, will need to specify per pickupable object in future
 		target.transform.parent = gameObject.transform;
-		target.rigidbody2D.fixedAngle = true;
-		target.collider2D.enabled = false;
+		//target.rigidbody2D.fixedAngle = true;
+		//target.collider2D.enabled = false;
 		pickedUp = target;
+		target.layer = LayerMask.NameToLayer ("Picked Up Object");
 	}
 
 	void Drop(){						//Drop the object
 		if (pickedUp == null)
 			return;
 		pickedUp.transform.parent = null;
-		pickedUp.rigidbody2D.fixedAngle = false;
-		pickedUp.collider2D.enabled = true;
+		Destroy (pickedUp.GetComponent<SpringJoint2D> ());
+		//pickedUp.rigidbody2D.fixedAngle = false;
+		//pickedUp.collider2D.enabled = true;
+		GameObject droppedObject = pickedUp;
+		Utility.doThis d = () => {
+			droppedObject.layer = LayerMask.NameToLayer ("Enemy");
+		};
+		StartCoroutine(Utility.wait (1f, d));
 		pickedUp = null;
 	}
 }
