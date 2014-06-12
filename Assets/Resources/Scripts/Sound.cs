5c851302c38cd41da87ef016bc1914d85d17cf80
Eric Anderson <hirorir@gmail.com>
2014-06-11 21:01:39 -0700

    moving some stuff
    sexy

---
+++ Assets/Resources/Scripts/Sound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Sound : MonoBehaviour {
+
+	public static AudioClip[] wessbat;
+	public static AudioClip[] villager;
+	// Use this for initialization
+	void Start () {
+		wessbat = Resources.LoadAll ("Audio/Wessbat") as AudioClip[];
+		villager = Resources.LoadAll ("Audio/Villagers") as AudioClip[];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
